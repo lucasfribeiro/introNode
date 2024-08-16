c986aae89e4195c107e8fe67f1bbd36f3eec517b
Lucas Ribeiro <135022338+lucasfribeiro@users.noreply.github.com>
2024-08-16 11:35:33 -0300

    Aula - 16/08

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -5,7 +5,52 @@ module.exports = {
         try {            
             return response.status(200).json({
                 sucesso: true, 
-                mensagem: 'Lista de usuários.', 
+                mensagem: 'Lista de pedido', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+    async cadastrarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: ' Cadastro de pedido', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+    async editarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Editar pedido', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+    async apagarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Apagar pedido', 
                 dados: null
             });
         } catch (error) {
---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+const express = require('express'); 
+const router = express.Router(); 
+
+// referência a controllers que serão utilizados nas rotas
+const UsuariosController = require('../controllers/usuarios');
+const PedidosController = require('../controllers/pedidos');
+
+// definição das rotas
+router.get('/usuarios', UsuariosController.listarUsuarios); 
+router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
+router.patch('/usuarios', UsuariosController.editarUsuarios); 
+router.delete('/usuarios', UsuariosController.apagarUsuarios);
+
+// post
+// patch
+// delete
+
+module.exports = router;
